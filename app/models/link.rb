bff710adea396088bb4469b948f70fd974c32714
Justin <justinmckelvey@hotmail.com>
2015-02-09 09:10:12 -0600

    links validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,7 @@ class Link < ActiveRecord::Base
 
   belongs_to :user
 
+  # validates :title, presence: true
+  # validates :url, presence: true
+  validates :url, :format => URI::regexp(%w(http https))
 end
